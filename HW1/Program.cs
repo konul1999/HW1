e9840e80eef38ae0c1ff1c03a0fe2d337f0a044b
konul1999 <konulfma@code.edu.az>
2025-07-21 03:17:23 +0400

    Add project files.

---
+++ HW1/Program.cs
@@ -0,0 +1,52 @@
+﻿//int num1 = 3;
+//int num2 = 15;
+//int num3 = 9;
+
+//int max = num1;
+
+//if ( num2 > max)
+//{
+//    max= num2;
+//}
+//if ( num3 > max)
+//{
+//    max = num3;
+//}
+//Console.WriteLine("En böyük eded: " + max);
+
+
+
+
+//int grade = 97;
+//if (grade >= 0 && grade < 65)
+//{
+//    Console.WriteLine("Kəsilib");
+//}
+//else if (grade >= 65 && grade <= 84)
+//{
+//    Console.WriteLine("Adi Diplom alıb");
+//}
+//else if (grade >= 85 && grade <= 94)
+//{
+//    Console.WriteLine("Şərəf Diplomu alıb");
+//}
+//else if (grade >= 95 && grade <= 100)
+//{
+//    Console.WriteLine("Yüksək Şərəf Diplomu alıb");
+//}
+//else
+//{
+//    Console.WriteLine("Qiymet 0-100 aralığında olmalıdır!");
+//}
+
+
+
+
+//int num = 25;
+
+//int ones = num % 10;      
+//int tens = num / 10;      
+
+//int sum = ones + tens;
+
+//Console.WriteLine("Rəqəmlər cəmi: " + sum);
